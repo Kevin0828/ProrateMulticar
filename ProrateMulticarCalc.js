e005f1ea38ebc65ff7ccf71b82ff8ca869e6d429
Kevin Fontela <fontela98@yahoo.com>
2020-07-09 01:32:52 -0400

    Update ProrateMulticarCalc.js

--- ProrateMulticarCalc.js
+++ ProrateMulticarCalc.js
@@ -1,39 +1,44 @@
-var totalCars = 2; // number of cars to calculate
-
+let MAX_CARS = 10;
+let MIN_CARS = 2;
 
 /**
-
    adds a car to the list
 */
 function addCar()
 {
-	totalCars++;
+	var carsCounter = (document.querySelectorAll(".car").length + 1);
 
-	var classElement = document.getElementById("cars-boxes-body");
-	var wrapTr = document.createElement("tr");
-	var wrapTd = document.createElement("td");
+   if(carsCounter <= MAX_CARS)
+   {
+	var table = document.getElementById("cars-boxes-body");
+	var row = table.insertRow(-1);
+    var cell = row.insertCell(-1);
+    var cell1 = row.insertCell(-1);
 	var label = document.createElement("label");
 	var input = document.createElement("INPUT");
-	var message = document.createElement("td")
+	var message = document.createElement("label")
 
 	message.setAttribute("class", "premium-text");
-	message.setAttribute("id", "premium-text-" + totalCars);
 	message.innerHTML = "Premium";
 
-	label.setAttribute("for", "car" + totalCars);
+	label.setAttribute("for", "car" + carsCounter);
 	label.setAttribute("class", "car-label");
-	label.innerHTML = "Car " + totalCars;
+	label.innerHTML = "Car " + carsCounter;
 
 	input.setAttribute("class", "car");
 	input.setAttribute("placeholder", "$0.00");
 	input.setAttribute("type", "text");
-	input.setAttribute("id", "car" + (totalCars - 1));
+	input.setAttribute("id", "car" + (carsCounter - 1));
 
-	classElement.appendChild(wrapTr);
-	wrapTr.appendChild(wrapTd);
-	wrapTd.appendChild(label);
-	wrapTd.appendChild(input);
-	wrapTr.appendChild(message);
+	cell.appendChild(label);
+	cell.appendChild(input);
+	cell1.appendChild(message);
+}
+else
+{
+      alert("You can only have a maximum of " + MAX_CARS + " cars");
+}
+
 }
 
 /**
@@ -41,14 +46,15 @@ function addCar()
 */
 function removeCar()
 {
-    if(totalCars > 2)
+	var carsCounter = (document.querySelectorAll(".car").length);
+
+    if(carsCounter > MIN_CARS)// minimum amount of cars
     {
-    	totalCars -= (totalCars > 0) ? 1 : 0;
-	document.getElementById("cars-boxes-body").deleteRow(totalCars);
-     }
+      	document.getElementById("cars-boxes-body").deleteRow(carsCounter - 1);
+    }
     else
     {
-    	alert(" You can only have a minimum of 2 cars");
+    	alert("You can only have a minimum of 2 cars");
     }
 }
 
@@ -76,8 +82,7 @@ function calculate()
 	var carsCounter = document.querySelectorAll(".car").length;
 	var downpayment = parseFloat(document.getElementById("downpayment").value);
 	var monthlypayment = parseFloat(document.getElementById("monthlypayment").value);
-
-        var totalSum = 0;
+    var totalSum = 0;
 	var arrPremium = [];
 	var carMonthly = [];
 	var carDown = [];
@@ -88,7 +93,6 @@ function calculate()
 		totalSum =  (arrPremium[i] + totalSum);
 	}
 	
-
 	for(var i = 0; i < carsCounter; ++i)
 	{
 		carMonthly[i] = ((arrPremium[i] / totalSum) * monthlypayment);
@@ -101,25 +105,43 @@ function calculate()
 
 function displayResults(downpayments, monthlypayments)
 {
-	var classElement = document.getElementById("modal-table-body");
+	var table = document.getElementById("modal-table-body");
 
    for(var i = 0; i < downpayments.length; ++i)
    { 
-   	 var wrapTr = document.createElement("tr");
-	 var wrapTd = document.createElement("td");
-	 var label = document.createElement("p");
+	 var row = table.insertRow(-1);
+     var car_cell = row.insertCell(-1);
+     var down_cell = row.insertCell(-1);
+     var mp_cell = row.insertCell(-1);
 
-	classElement.appendChild(wrapTr);
-	wrapTr.appendChild(wrapTd);
-	wrapTd.appendChild(label);
+	 var car_label = document.createElement("p");
+	 car_label.setAttribute("class", "premium-text modal-results");
+	 car_cell.appendChild(car_label);
 
-	label.innerHTML = "Car (" + (i + 1) + ") downpayment $" + downpayments[i].toFixed(2) +
-	 "-- Montly payment " + "$" + monthlypayments[i].toFixed(2);
+	 var down_label = document.createElement("p");
+	 down_label.setAttribute("class", "premium-text modal-results");
+	 down_cell.appendChild(down_label);
+
+	 var mp_label = document.createElement("p");
+	 mp_label.setAttribute("class", "premium-text modal-results");
+	 mp_cell.appendChild(mp_label);
+
+	 downpayments[i] = (downpayments[i] > 0) ? downpayments[i] : 0.00;
+	 monthlypayments[i] = (monthlypayments[i] > 0) ? monthlypayments[i] : 0.00;
+
+	 car_label.innerHTML = "Vehicle " + (i + 1);
+     down_label.innerHTML = "$" + downpayments[i].toFixed(2);
+	 mp_label.innerHTML = "$" + monthlypayments[i].toFixed(2);
    }
-
-
 }
 
+
+function resetValues()
+{
+	document.getElementById("downpayment").innerHTML = '';
+	document.getElementById("monthlypayment").innerHTML = '';
+	document.getElementsByClassName("car").innerHTML = '';
+}
 
 
 
